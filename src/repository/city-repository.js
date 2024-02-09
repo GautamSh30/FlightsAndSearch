f409b9844aff175f6120526c0be18aa7863730fd
Gautam Sharma <gautam2586272@gmail.com>
2024-02-10 00:16:43 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,11 +1,14 @@
 const { City } = require("../models/index");
 
 class CityRepository {
+  // constructor() {}
+
   async createCity({ name }) {
     try {
       const city = await City.create({ name });
       return city;
     } catch (err) {
+      console.log("Something went wrong in the repository layer");
       throw { err };
     }
   }
@@ -17,7 +20,33 @@ class CityRepository {
           id: cityId,
         },
       });
+      return true;
     } catch (err) {
+      console.log("Something went wrong in the repository layer");
+      throw { err };
+    }
+  }
+
+  async UpdateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (err) {
+      console.log("Something went wrong in the repository layer");
+      throw { err };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return City;
+    } catch (err) {
+      console.log("Something went wrong in the repository layer");
       throw { err };
     }
   }
