483a5fe03f566ab02424a6ccbbf1f59c3011f15f
Gautam Sharma <gautam2586272@gmail.com>
2024-02-12 17:05:20 +0530

    Added airplanes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const { PORT } = require("./config/serverConfig");
 const ApiRoutes = require("./routes/index");
 
 const db = require("./models/index");
+const { Airplane } = require("./models/index");
 
 const setupAndStartServer = async () => {
   const app = express();
@@ -17,21 +18,9 @@ const setupAndStartServer = async () => {
   app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
 
-    if (process.env.sync_db) {
-      try {
-        await db.sequelize.sync({ alter: true });
-      } catch (error) {
-        console.error("Error during synchronization:", error);
-      }
-    }
-
-    // const city = await City.findOne({
-    //   where: {
-    //     id: 9,
-    //   },
-    // });
-    // const airport = await city.getAirports();
-    // console.log(city, airport);
+    // if (process.env.sync_db) {
+    //   db.sequelize.sync({ alter: true });
+    // }
   });
 };
 
